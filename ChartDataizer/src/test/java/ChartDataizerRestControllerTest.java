899c94d2e4926d694584bb1660ff65c90c05aa92
Athan Soulis <athan.soulis@gmail.com>
2018-03-13 11:56:36 +0200

    Test for the ChartDataizer Rest Controller
    Added test resources, removed some unneeded ones. All tests are passing.

---
+++ ChartDataizer/src/test/java/ChartDataizerRestControllerTest.java
@@ -0,0 +1,92 @@
+import JsonChartRepresentation.QueryInfo;
+import RestControllers.ChartDataizerRestController;
+
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.TestContext;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.ResultActions;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.web.context.WebApplicationContext;
+
+import java.net.URL;
+
+import static org.mockito.Mockito.mock;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest(webEnvironment= SpringBootTest.WebEnvironment.DEFINED_PORT)
+@EnableAutoConfiguration
+@ContextConfiguration(classes={TestContext.class,ChartDataizerRestController.class})
+public class ChartDataizerRestControllerTest {
+
+    @Autowired
+    private WebApplicationContext context;
+
+    private MockMvc mockMvc;
+
+    @Before
+    public void setUp(){
+        mockMvc = MockMvcBuilders.webAppContextSetup(context).build();
+    }
+
+    @Test
+    public void highchartsLibPost() throws Exception {
+
+        QueryInfo mockHichchartsQueryInfo = new QueryInfo();
+        mockHichchartsQueryInfo.setLibrary("Highcharts");
+
+        ObjectMapper mapper = new ObjectMapper();
+        String jsonQueryInfo = mapper.writeValueAsString(mockHichchartsQueryInfo);
+
+        this.mockMvc.perform(MockMvcRequestBuilders.post("/chart").content(jsonQueryInfo)
+                .contentType(MediaType.APPLICATION_JSON_UTF8_VALUE)
+                .accept(MediaType.APPLICATION_JSON_UTF8_VALUE))
+                .andExpect(MockMvcResultMatchers.status().isOk())
+                .andExpect(MockMvcResultMatchers.content().contentType(MediaType.APPLICATION_JSON_UTF8_VALUE));
+    }
+
+    @Test
+    public void unknownLibPost() throws Exception {
+
+        QueryInfo mockUnknownQueryInfo = new QueryInfo();
+        mockUnknownQueryInfo.setLibrary("Unknown");
+
+        ObjectMapper mapper = new ObjectMapper();
+        String jsonQueryInfo = mapper.writeValueAsString(mockUnknownQueryInfo);
+
+        this.mockMvc.perform(MockMvcRequestBuilders.post("/chart").content(jsonQueryInfo)
+                .contentType(MediaType.APPLICATION_JSON_UTF8_VALUE)
+                .accept(MediaType.APPLICATION_JSON_UTF8_VALUE))
+                .andExpect(MockMvcResultMatchers.status().isUnprocessableEntity());
+    }
+
+    @Test
+    public void PostResponse() throws Exception {
+
+        ObjectMapper mapper = new ObjectMapper();
+        JsonNode jsonNode = mapper.readTree(new URL("http://localhost:8080/jsonFiles/multiAdminToHtml.json"));
+
+        ResultActions ra = this.mockMvc.perform(MockMvcRequestBuilders.post("/chart").content(jsonNode.toString())
+                .contentType(MediaType.APPLICATION_JSON_UTF8_VALUE)
+                .accept(MediaType.APPLICATION_JSON_UTF8_VALUE))
+                .andExpect(MockMvcResultMatchers.status().isOk())
+                .andExpect(MockMvcResultMatchers.content().contentType(MediaType.APPLICATION_JSON_UTF8_VALUE))
+                .andExpect(MockMvcResultMatchers.jsonPath("@.series[0].data").exists())
+                .andExpect(MockMvcResultMatchers.jsonPath("@.series[0].data").isNotEmpty());
+
+        System.out.println(ra.andReturn().getResponse().getContentAsString());
+
+    }
+}
