7d7c4eff143c99349f68e5162f9a67cc75038897
tsampikos <tsampikos92@gmail.com>
2018-09-25 16:26:42 +0300

    deploy to icm #yolo

--- Application/src/main/java/gr/uoa/di/madgik/StatisticChartGenerator_App/appBoot.java
+++ Application/src/main/java/gr/uoa/di/madgik/StatisticChartGenerator_App/appBoot.java
@@ -2,11 +2,13 @@ package gr.uoa.di.madgik.StatisticChartGenerator_App;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.support.SpringBootServletInitializer;
 import org.springframework.context.annotation.ComponentScan;
 
 @ComponentScan(basePackages = {"gr/uoa/di/madgik/ChartDataFormatter", "gr/uoa/di/madgik/statstool"} )
 @SpringBootApplication
-public class appBoot {
+//public class appBoot {
+public class appBoot extends SpringBootServletInitializer {
 
     public static void main(String[] args) {
 
