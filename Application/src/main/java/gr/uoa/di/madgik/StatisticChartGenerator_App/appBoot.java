fafc6e027fb2e7588958dc23687b2201de8e55b5
Konstantinos Spyrou <spyroukon@gmail.com>
2019-02-28 13:02:13 +0200

    read 'application.properties' file from 'statsConfig' folder inside tomcat/lib

--- Application/src/main/java/gr/uoa/di/madgik/StatisticChartGenerator_App/appBoot.java
+++ Application/src/main/java/gr/uoa/di/madgik/StatisticChartGenerator_App/appBoot.java
@@ -2,16 +2,30 @@ package gr.uoa.di.madgik.StatisticChartGenerator_App;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.support.SpringBootServletInitializer;
-import org.springframework.context.annotation.ComponentScan;
 
-@ComponentScan(basePackages = {"gr/uoa/di/madgik/ChartDataFormatter", "gr/uoa/di/madgik/statstool"} )
+import java.util.Properties;
+
+//@ComponentScan(basePackages = {"gr/uoa/di/madgik/ChartDataFormatter", "gr/uoa/di/madgik/statstool"})
 @SpringBootApplication
-//public class appBoot {
 public class appBoot extends SpringBootServletInitializer {
 
     public static void main(String[] args) {
 
         SpringApplication.run(appBoot.class, args);
+    }
+
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder springApplicationBuilder) {
+        return springApplicationBuilder
+                .sources(appBoot.class)
+                .properties(getProperties());
+    }
+
+    static Properties getProperties() {
+        Properties props = new Properties();
+        props.put("spring.config.location", "classpath:statsConfig/");
+        return props;
     }
 }
