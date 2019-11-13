8c8e33b32a310001bd09d93bb0617d3cf536c63f
Antonis Lempesis <antleb@di.uoa.gr>
2019-11-13 15:38:32 +0200

    added support for namedqueries

--- DBAccess/src/main/java/gr/uoa/di/madgik/statstool/services/StatsServiceImpl.java
+++ DBAccess/src/main/java/gr/uoa/di/madgik/statstool/services/StatsServiceImpl.java
@@ -1,8 +1,11 @@
 package gr.uoa.di.madgik.statstool.services;
 
 import gr.uoa.di.madgik.statstool.mapping.Mapper;
+import gr.uoa.di.madgik.statstool.repositories.NamedQueryRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -16,37 +19,66 @@ import gr.uoa.di.madgik.statstool.repositories.StatsRepository;
 @Service
 public class StatsServiceImpl implements StatsService{
 
+    @Autowired
     private StatsRepository statsRepository;
 
+    @Autowired
     private StatsRedisRepository statsRedisRepository;
 
+    @Autowired
+    private NamedQueryRepository namedQueryRepository;
+
+    @Autowired
     private Mapper mapper;
 
     private final Logger log = Logger.getLogger(this.getClass());
 
-    public StatsServiceImpl(StatsRepository statsRepository, StatsRedisRepository statsRedisRepository, Mapper mapper) {
-        this.statsRepository = statsRepository;
-        this.statsRedisRepository = statsRedisRepository;
-        this.mapper = mapper;
-    }
-
     @Override
     public List<Result> query(List<Query> queryList) {
         List<Result> results = new ArrayList<>();
-        for(Query query : queryList) {
-            List<Object> parameters = new ArrayList<>();
-            String querySql = mapper.map(query, parameters);
-            String fullSqlQuery = statsRepository.getFullQuery(querySql, parameters);
-            log.info("SQL: " + fullSqlQuery);
-            Result result = statsRedisRepository.get(fullSqlQuery);
-            if(result != null) {
-                results.add(result);
-                continue;
+        List<Object> parameters = new ArrayList<>();
+
+        for (Query query : queryList) {
+
+            log.info("query: " + query);
+
+            String queryName = query.getName();
+            Result result;
+
+            if (queryName == null) {
+                String querySql = mapper.map(query, parameters);
+                String fullSqlQuery = statsRepository.getFullQuery(querySql, parameters);
+
+                log.info("SQL: " + fullSqlQuery);
+
+                result = statsRedisRepository.get(fullSqlQuery);
+
+                if (result == null) {
+                    result = statsRepository.executeQuery(querySql, parameters);
+                    statsRedisRepository.save(fullSqlQuery, result);
+                }
+            } else {
+                String querySql = getNamedQuery(queryName);
+                result = statsRedisRepository.get(querySql);
+
+                if (result == null) {
+                    result = statsRepository.executeQuery(querySql, new ArrayList<>());
+                    statsRedisRepository.save(querySql, result);
+                }
             }
-            result = statsRepository.executeQuery(querySql, parameters);
-            statsRedisRepository.save(fullSqlQuery, result);
+
             results.add(result);
         }
         return results;
+    }
+
+    private String getNamedQuery(String queryName) {
+        try {
+            return namedQueryRepository.getQuery(queryName);
+        } catch (IOException e) {
+            log.error("Error getting named query", e);
+
+            return null;
+        }
     }
 }
