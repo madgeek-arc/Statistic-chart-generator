f2cda6e05977010a94acc0aee8207e5e66e08517
antleb <antleb@di.uoa.gr>
2020-05-06 00:02:44 +0000

    upgraded to spring boot 2.1.2

--- Application/src/main/java/gr/uoa/di/madgik/StatisticChartGenerator_App/appBoot.java
+++ Application/src/main/java/gr/uoa/di/madgik/StatisticChartGenerator_App/appBoot.java
@@ -3,7 +3,7 @@ package gr.uoa.di.madgik.StatisticChartGenerator_App;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
-import org.springframework.boot.web.support.SpringBootServletInitializer;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.context.annotation.ComponentScan;
 
 import java.util.Properties;
