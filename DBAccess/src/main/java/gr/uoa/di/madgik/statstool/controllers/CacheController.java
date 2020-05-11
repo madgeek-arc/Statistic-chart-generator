b074d284e10a792a58fb8b9a974362f657d69875
Antonis Lempesis <antleb@di.uoa.gr>
2020-05-12 00:41:13 +0300

    promote is in the controller too

--- DBAccess/src/main/java/gr/uoa/di/madgik/statstool/controllers/CacheController.java
+++ DBAccess/src/main/java/gr/uoa/di/madgik/statstool/controllers/CacheController.java
@@ -12,9 +12,13 @@ public class CacheController {
     @Autowired
     private CacheService cacheService;
 
-//    @RequestMapping(value = "compute")
     @GetMapping("compute")
     public void computeNumbers() throws StatsServiceException {
         cacheService.calculateNumbers();
+    }
+
+    @GetMapping("promote")
+    public void promoteNumbers() {
+        cacheService.promoteNumbers();
     }
 }
