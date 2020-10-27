5a5c75ab97e044acd6b77d84c2139f9d871463bb
antleb <antleb@di.uoa.gr>
2020-10-27 14:36:01 +0200

    added supported types of misc category

--- ChartDataFormatter/src/main/java/gr/uoa/di/madgik/ChartDataFormatter/DataFormatter/SupportedDiagramsService.java
+++ ChartDataFormatter/src/main/java/gr/uoa/di/madgik/ChartDataFormatter/DataFormatter/SupportedDiagramsService.java
@@ -4,6 +4,7 @@ import gr.uoa.di.madgik.ChartDataFormatter.Handlers.SupportedLibraries;
 import org.springframework.stereotype.Service;
 
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 
 import static gr.uoa.di.madgik.ChartDataFormatter.DataFormatter.SupportedChartTypes.*;
@@ -15,26 +16,37 @@ public class SupportedDiagramsService {
     private List<SupportedChart> supportedCharts;
     private List<SupportedMap> supportedMaps;
     private List<SupportedSpecialDiagram> supportedSpecialDiagrams;
+    private List<SupportedMisc> supportedMiscs;
 
     public SupportedDiagramsService() {
         this.initSupportedCharts();
         this.initSupportedMaps();
         this.initSupportedSpecialDiagrams();
+        this.initSupportedMisc();
     }
 
     public List<SupportedChart> getSupportedCharts() { return supportedCharts; }
     public List<SupportedMap> getSupportedMaps() { return supportedMaps; }
     public List<SupportedSpecialDiagram> getSupportedSpecialDiagrams() { return supportedSpecialDiagrams; }
+    public List<SupportedMisc> getSupportedMiscs() { return this.supportedMiscs; }
 
-    public class SupportedChart {
-        public SupportedChartTypes type;
-        public List<SupportedLibraries> supportedLibraries;
-
-        public SupportedChart(SupportedChartTypes type, List<SupportedLibraries> supportedLibraries) {
-            this.type = type;
-            this.supportedLibraries = supportedLibraries;
-        }
+    private void initSupportedMaps() {
+        this.supportedMaps = new ArrayList<>();
+        List<SupportedLibraries> mapLibs = new ArrayList<>();
+        mapLibs.add(HighMaps);
+        this.supportedMaps.add(new SupportedMap("world", "custom/world-robinson-highres", mapLibs));
     }
+
+    private void initSupportedSpecialDiagrams() {
+        this.supportedSpecialDiagrams = new ArrayList<>();
+        List<SupportedLibraries> chartLibs = new ArrayList<>();
+        chartLibs.add(HighCharts);
+        chartLibs.add(GoogleCharts);
+        chartLibs.add(eCharts);
+        this.supportedSpecialDiagrams.add(new SupportedSpecialDiagram("combo", chartLibs));
+
+    }
+
     private void initSupportedCharts() {
         this.supportedCharts = new ArrayList<>();
         List<SupportedLibraries> chartLibs = new ArrayList<>();
@@ -48,6 +60,21 @@ public class SupportedDiagramsService {
         this.supportedCharts.add(new SupportedChart(line,chartLibs));
     }
 
+    private void initSupportedMisc() {
+        this.supportedMiscs = new ArrayList<>();
+        this.supportedMiscs.add(new SupportedMisc("numbers", Collections.emptyList()));
+    }
+
+    public class SupportedChart {
+        public SupportedChartTypes type;
+        public List<SupportedLibraries> supportedLibraries;
+
+        public SupportedChart(SupportedChartTypes type, List<SupportedLibraries> supportedLibraries) {
+            this.type = type;
+            this.supportedLibraries = supportedLibraries;
+        }
+    }
+
     public class SupportedMap {
         public String type;
         public String name;
@@ -59,12 +86,6 @@ public class SupportedDiagramsService {
             this.supportedLibraries = supportedLibraries;
         }
     }
-    private void initSupportedMaps() {
-        this.supportedMaps = new ArrayList<>();
-        List<SupportedLibraries> mapLibs = new ArrayList<>();
-        mapLibs.add(HighMaps);
-        this.supportedMaps.add(new SupportedMap("world", "custom/world-robinson-highres", mapLibs));
-    }
 
     public class SupportedSpecialDiagram {
         public String type;
@@ -75,14 +96,15 @@ public class SupportedDiagramsService {
             this.supportedLibraries = supportedLibraries;
         }
     }
-    private void initSupportedSpecialDiagrams() {
-        this.supportedSpecialDiagrams = new ArrayList<>();
-        List<SupportedLibraries> chartLibs = new ArrayList<>();
-        chartLibs.add(HighCharts);
-        chartLibs.add(GoogleCharts);
-        chartLibs.add(eCharts);
-        this.supportedSpecialDiagrams.add(new SupportedSpecialDiagram("combo", chartLibs));
 
+    public class SupportedMisc {
+        public String type;
+        public List<SupportedLibraries> supportedLibraries;
+
+       public SupportedMisc(String type, List<SupportedLibraries> supportedLibraries) {
+           this.type = type;
+           this.supportedLibraries = supportedLibraries;
+       }
     }
 }
 
