cbe985bee434f914b447095ca75ca86d5e559c4f
antleb <antleb@di.uoa.gr>
2020-11-23 17:19:03 +0200

    updating and promoting cache in the same step

--- DBAccess/src/main/java/gr/uoa/di/madgik/statstool/services/CacheService.java
+++ DBAccess/src/main/java/gr/uoa/di/madgik/statstool/services/CacheService.java
@@ -2,8 +2,6 @@ package gr.uoa.di.madgik.statstool.services;
 
 public interface CacheService {
 
-    void promoteCache();
-
     public void calculateNumbers() throws StatsServiceException;
 
     public void promoteNumbers();
