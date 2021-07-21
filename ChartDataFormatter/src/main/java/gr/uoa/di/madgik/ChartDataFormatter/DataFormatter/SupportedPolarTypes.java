2f6a89dff75481c2f06e86b89eefaa6561a8c5b0
AthanSoulis <athan.soulis@gmail.com>
2021-07-21 18:24:59 +0300

    Added supported Polar diagrams

---
+++ ChartDataFormatter/src/main/java/gr/uoa/di/madgik/ChartDataFormatter/DataFormatter/SupportedPolarTypes.java
@@ -0,0 +1,9 @@
+package gr.uoa.di.madgik.ChartDataFormatter.DataFormatter;
+
+/**
+ * An enumeration of the currently supported Polar chart Types by every {@link gr.uoa.di.madgik.ChartDataFormatter.Handlers.SupportedLibraries}.
+ * This enumeration should be expanded with each new Polar type.
+ */
+public enum SupportedPolarTypes {
+    area,bar,column,line
+}
