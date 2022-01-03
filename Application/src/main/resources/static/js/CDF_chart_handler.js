9678649d5b6cb552462c9cde5da0fbaeedf5d0bc
AthanSoulis <athan.soulis@gmail.com>
2022-01-03 15:17:45 +0200

    Adding heatmap library for Highcharts v2

--- Application/src/main/resources/static/js/CDF_chart_handler.js
+++ Application/src/main/resources/static/js/CDF_chart_handler.js
@@ -128,7 +128,7 @@ function handleAdminSideData(dataJSONobj)
         //Dynamically add JS library
         loadJS("//code.highcharts.com/highcharts.js",
         () => loadJS("//code.highcharts.com/highcharts-more.js",
-        () => loadJS("//code.highcharts.com/modules/heatmap.js"),
+        () => loadJS("//code.highcharts.com/modules/heatmap.js",
         () => loadJS("//code.highcharts.com/modules/treemap.js",
         () => loadJS("//code.highcharts.com/modules/drilldown.js",
         () => loadJS("//code.highcharts.com/modules/no-data-to-display.js",
@@ -174,7 +174,7 @@ function handleAdminSideData(dataJSONobj)
                         domainLink+"/chart");
         }
             
-        )))))))));
+        ))))))))));
         
         break;
     }
