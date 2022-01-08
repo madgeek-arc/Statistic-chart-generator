4486e98f836ec9b3e5990e153e16266b563ff2b5
Athan Soulis <athan.soulis@gmail.com>
2022-01-08 19:47:27 +0200

    Updated the Yaml diagram properties test

--- ChartDataFormatter/src/test/java/gr/uoa/di/madgik/ChartDataFormatter/YamlDiagramPropertiesTest.java
+++ ChartDataFormatter/src/test/java/gr/uoa/di/madgik/ChartDataFormatter/YamlDiagramPropertiesTest.java
@@ -8,34 +8,35 @@ import static org.junit.jupiter.api.Assertions.assertNull;
 import org.junit.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.boot.test.context.ConfigDataApplicationContextInitializer;
+import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.TestPropertySource;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 
 import gr.uoa.di.madgik.ChartDataFormatter.DataFormatter.SupportedDiagrams.POJOs.SupportedDiagrams;
 import gr.uoa.di.madgik.ChartDataFormatter.Handlers.SupportedLibraries;
 
 @ExtendWith(SpringExtension.class)
+@SpringBootTest
 @ContextConfiguration(initializers = ConfigDataApplicationContextInitializer.class)
-@EnableConfigurationProperties(value = SupportedDiagrams.class)
 public class YamlDiagramPropertiesTest {
  
     @Autowired
     private SupportedDiagrams diagrams;
  
     @Test
-    public void whenYamlNestedLists_thenLoadComplexLists() {
+    public void SupportedDiagramsReadFromYaml() {
         
         assertNotNull(diagrams, "Diagrams not initialized");
-        assertNotNull(diagrams.getSupportedCharts(), "Supported Charts are not null");
-        assertEquals(diagrams.getSupportedCharts().get(0).getSupportedLibraries().get(1), SupportedLibraries.GoogleCharts);
-        assertFalse(diagrams.getSupportedCharts().get(0).isPolar);
+        assertNotNull(diagrams.getCharts(), "Supported Charts are not null");
+        assertEquals(diagrams.getCharts().get(0).getSupportedLibraries().get(1), SupportedLibraries.GoogleCharts);
+        assertFalse(diagrams.getCharts().get(0).isPolar);
         
-        assertNull(diagrams.getSupportedPolars());
-        assertNull(diagrams.getSupportedMaps());
-        assertNull(diagrams.getSupportedMiscs());
-        assertNull(diagrams.getSupportedSpecialDiagrams());
+        assertNull(diagrams.getPolars());
+        assertNull(diagrams.getMaps());
+        assertNull(diagrams.getMiscs());
+        assertNull(diagrams.getSpecials());
         
     }
 	
